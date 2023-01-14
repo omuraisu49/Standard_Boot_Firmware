6e34f110efa98a954192ce9109573a996c47ea51
omuraisu49 <27958182+omuraisu49@users.noreply.github.com>
2023-01-15 01:39:35 +0900

    Fix notes

--- sample/sample_code_simple.c
+++ sample/sample_code_simple.c
@@ -10,6 +10,7 @@ void hal_entry(void)
     fsp_err_t fsp_err;
 
     /* Open driver */
+    /* Note: IOPORT and SCI module must be initialized before the boot firmware driver open */ 
     STDBOOT_Open(&g_stdboot0_ctrl, &g_stdboot0_cfg);
 
     /* Open serial command interface */
@@ -17,25 +18,28 @@ void hal_entry(void)
     R_BSP_SoftwareDelay(200, BSP_DELAY_UNITS_MILLISECONDS);
 
     fsp_err = STDBOOT_TargetSettingUp(&g_stdboot0_ctrl);
-    if(FSP_SUCCESS != fsp_err) __BKPT(0);
+    if(FSP_SUCCESS != fsp_err){ __BKPT(0);}
 
     /* Send inquiry command */
     fsp_err = STDBOOT_InquiryCommandCheck(&g_stdboot0_ctrl, 1000);
-    if(FSP_SUCCESS != fsp_err) __BKPT(0);
+    if(FSP_SUCCESS != fsp_err){ __BKPT(0);}
 
     /* Read area information */
     fsp_err = STDBOOT_AreaInformationRead(&g_stdboot0_ctrl, &stdboot_memory_info[0], 2, 1000);
-    if(FSP_SUCCESS != fsp_err) __BKPT(0);
+    if(FSP_SUCCESS != fsp_err){ __BKPT(0);}
 
-    /* Read 0x0~1124KB */
+    /* Read memory 0x0~1124B */
     fsp_err = STDBOOT_MemoryRead(&g_stdboot0_ctrl, 0x0, 1124, &firmare_read_data[0], 10000);
-    if(FSP_SUCCESS != fsp_err) __BKPT(0);
+    if(FSP_SUCCESS != fsp_err){ __BKPT(0);}
     memcpy(&firmare_read_data_u32[0], &firmare_read_data[0], 0x400);
 
     /* Read current DLM state */
-    device_lifecycle_management_state_t current_dlm_state;
-    fsp_err = STDBOOT_DLMStateRead(&g_stdboot0_ctrl, &current_dlm_state, 1000);
-    if(FSP_SUCCESS != fsp_err) __BKPT(0);
+    if((g_stdboot0_cfg.system_type == SYSTEM_TYPE_RA_CM33) || (g_stdboot0_cfg.system_type == SYSTEM_TYPE_RA_CM33_ENTRY))
+    {
+        device_lifecycle_management_state_t current_dlm_state;
+        fsp_err = STDBOOT_DLMStateRead(&g_stdboot0_ctrl, &current_dlm_state, 1000);
+        if(FSP_SUCCESS != fsp_err){ __BKPT(0);}
+    }
 
     /* Close serial command interface */
     STDBOOT_TargetBootModeExit(&g_stdboot0_ctrl);
